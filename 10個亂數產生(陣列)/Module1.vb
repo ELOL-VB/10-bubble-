13c55828fdbc66305421a8cc4134c35bec9af426
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-10 21:42:44 +0800

    Add files via upload

---
+++ 10個亂數產生(陣列)/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Randomize()
+        Dim a(5) As Integer
+        Dim x, i, j, e As Integer
+        Dim str, p As String
+        str = ""
+        x = 0
+        For x = 0 To 5
+            a(x) = Int(Rnd() * 49) + 1
+            str = a(x) & "  " & str
+        Next
+        For i = 1 To UBound(a)
+            For j = 0 To UBound(a) - i
+                If a(j) > a(j + 1) Then
+                    e = a(j)
+                    a(j) = a(j + 1)
+                    a(j + 1) = e
+                End If
+            Next
+        Next
+
+        Console.WriteLine("未排序:" & str)
+        For i = 0 To UBound(a)
+            Console.Write(a(i) & " ")
+        Next
+
+
+        Console.Read()
+    End Sub
+
+End Module
